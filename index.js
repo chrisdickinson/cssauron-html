ee55b578c4c20879381af39cc2731e88087b9d34
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-12-27 15:45:55 -0800

    add shims for older browsers

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 module.exports = require('cssauron')({
   tag: function(node) { return (node.tagName || '').toLowerCase() }
-, contents: function(node) { return node.innerText || node.innerHTML }
+, contents: function(node) { return node.innerText || node.textContent || '' }
 , id: 'id'
 , "class": 'className'
 , parent: 'parentNode'
-, children: 'children'
+, children: function(node) {
+    if(node.children) {
+      return node.children
+    }
+
+    var out = []
+    for(var i = 0, len = node.childNodes.length; i < len; ++i) {
+      if(node.childNodes[i].tagName) out.push(node.childNodes[i])
+    } 
+    return out
+  }
 , attr: function(node, attr) { return node.getAttribute(attr) }
 })
